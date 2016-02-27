c4b36d1b545cc9965e0eccd3e43e19e3dff31b00
Gabe-Kubiak <gabekubiak@gmail.com>
2016-02-26 21:19:06 -0500

    Take some code!!!
    finished porticullis again! Mr. W checked it over so we know it has
    correct logic.

--- src/com/github/adambots/stronghold2016/auton/Barrier_Porticullis.java
+++ src/com/github/adambots/stronghold2016/auton/Barrier_Porticullis.java
@@ -2,47 +2,66 @@ package com.github.adambots.stronghold2016.auton;
 
 import org.usfirst.frc.team245.robot.Actuators;
 import org.usfirst.frc.team245.robot.Sensors;
-
 import com.github.adambots.stronghold2016.arm.Arm;
 import com.github.adambots.stronghold2016.drive.Drive;
-
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 
 public class Barrier_Porticullis extends Barrier {
-	static int counter;
-	static int clearanceTime = 20;// Susceptible to change
-	static int armIncrement;
+	static boolean raisePosition = false;
+	static boolean crossedPosition = false;
 
-	public static void Porticullis(double raise, double speed) {
-		boolean position = false;
-		double tolerance = 0;
-		if (position == false) {
-			Drive.drive(speed);
-
+	public static boolean Porticullis(double raise) {
+		boolean raisePosition = false;
+		// double tolerance = 0; // gyro stuuf
+		double armIncrementalSpeed = .5;
+		double distance = 24;// to gate type thingamabob
+		double distance2 = 60;// inches from midpoint of defense when arm raised
+		if (Sensors.getArmMaxLimitSwitch().get() == true) {
+			Actuators.getArmAngleMotor().set(0);
 		}
-		if (position == true)
+		if (raisePosition == false) {
+			Drive.driveWithPID(distance, distance);
+		}
+		if (Actuators.getLeftDriveMotor().getError() <= 20) {
+			raisePosition = true;
 			if (Sensors.getStringPotArmAngle() < raise) {
-				Arm.moveArm(armIncrement);// Arm speed may change
+				Arm.moveArm(armIncrementalSpeed);// Arm speed may change
 			} else {
-				Drive.drive(speed);
-				// needs positioning
-				if (Sensors.getRobotGyro().getAngle() > -tolerance) {
-
-				}
-			}
-		// needs positioning
-		if (Sensors.getRobotGyro().getAngle() > -tolerance && Sensors.getRobotGyro().getAngle() < tolerance) {
-			counter++;
-			if (counter >= clearanceTime) {
-				Drive.drive(speed);
-			}
-			if (counter >= clearanceTime && Sensors.getRobotGyro().getAngle() > -tolerance
-					&& Sensors.getRobotGyro().getAngle() < tolerance) {
-				if (Sensors.getArmMinLimitSwitch().get() == false) {
-					Arm.moveArm(-armIncrement);// might need to change increment
+				Drive.driveWithPID(distance2, distance2);
+				if (Actuators.getLeftDriveMotor().getError() <= 20) {
+					crossedPosition = true;
+					return true;
 				}
 			}
 		}
+		return false;
+
+		// EXPERIMENTAL GYRO DEFENSE CROSSING PLZ IGNORE
+		// if (position == true)
+		// if (Sensors.getStringPotArmAngle() < raise) {
+		// Arm.moveArm(armIncrement);// Arm speed may change
+		// } else {
+		// Drive.driveWithPID(distance2,distance2);
+		// // needs positioning
+		// if (Sensors.getRobotGyro().getAngle() > -tolerance) {
+		//
+		// }
+		// }
+		//
+		// if (Sensors.getRobotGyro().getAngle() > -tolerance &&
+		// Sensors.getRobotGyro().getAngle() < tolerance) {
+		// counter++;
+		// if (counter >= clearanceTime) {
+		// Drive.drive(speed);
+		// }
+		// if (counter >= clearanceTime && Sensors.getRobotGyro().getAngle()
+		// > -tolerance
+		// && Sensors.getRobotGyro().getAngle() < tolerance) {
+		// if (Sensors.getArmMinLimitSwitch().get() == false) {
+		// Arm.moveArm(-armIncrement);// might need to change increment
+		// }
+		// }
+		// }
 
 	}
 }
